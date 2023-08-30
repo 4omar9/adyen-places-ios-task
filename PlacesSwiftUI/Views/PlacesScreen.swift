ba2177d4ae608d7c5a926ea9ae7543691f679541
4omar9 <omasri@omarmasri.com>
2023-08-30 08:38:38 -0500

    First commit

---
+++ PlacesSwiftUI/Views/PlacesScreen.swift
@@ -0,0 +1,20 @@
+//
+//  PlacesScreen.swift
+//  Places
+//
+//
+
+import SwiftUI
+import AdyenNetworking
+
+struct PlacesScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PlacesScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        PlacesScreen()
+    }
+}
